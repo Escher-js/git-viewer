9a2b410e1515f753bece8daaf19ee5a288367bce
psmuler <zhmuler@gmail.com>
2023-05-17 09:29:25 +0900

    test commit

--- src/ShowRepositories.js
+++ src/ShowRepositories.js
@@ -7,8 +7,13 @@ const ShowRepositories = ({ username }) => {
     useEffect(() => {
         const fetchRepos = async () => {
             try {
-                const response = await axios.get(`https://api.github.com/users/${username}/repos`);
-                setRepos(response.data);
+                const response = await axios.get(`https://api.github.com/user/repos`, {
+                    headers: {
+                        'Authorization': `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+                    },
+                });
+                const privateRepos = response.data.filter(repo => repo.private);
+                setRepos(privateRepos);
             } catch (error) {
                 console.error(error);
             }
@@ -19,7 +24,7 @@ const ShowRepositories = ({ username }) => {
 
     return (
         <div>
-            <h2>{username}'s Repositories</h2>
+            <h2>{username}'s Private Repositories</h2>
             {repos.map(repo => (
                 <div key={repo.id}>
                     <h3>{repo.name}</h3>
